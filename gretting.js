2c4098dc7ea0435386418fa2d05f6c8c8123eb59
ejxzhn22 <ejxzhn22@naver.com>
2020-01-26 22:36:37 +0900

    clock, greeting
    clock, greeting

---
+++ gretting.js
@@ -0,0 +1,25 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+function init() {
+  loadName();
+}
+
+init();
